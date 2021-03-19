8bc911f27295f16645ac393d2be285da96a2bf95
GabZeraik <58312589+GabZeraik@users.noreply.github.com>
2021-03-19 14:38:04 -0300

    Commit final sem paginação

--- script.js
+++ script.js
@@ -1,3 +1,52 @@
+const msgPai = document.querySelector('.js-requisicao');
+
+class Button {
+    constructor(classe, textContent, funcao) {
+        this.tag = "button";
+        this.classe = classe;
+        this.textContent = textContent;
+        this.funcao = funcao;
+    }
+    createElButton() {
+        return `<${this.tag} class="${this.classe}" onclick="${this.funcao}">${this.textContent}</${this.tag}>`;
+    }
+}
+
+class Card {
+    constructor(planeta) {
+        this.planeta = planeta;
+        this.buttonEditar = new Button('card__heading-button js-editar', 'Editar', 'addEditar(this)');
+        this.buttonExcluir = new Button('card__heading-button js-excluir', 'Excluir', 'addRemover(this)');
+    }
+    createElCard() {
+        return `<div class="card__side card__side--back js-card-back">
+        <div class="card__cover">
+            ${this.buttonExcluir.createElButton()}
+            ${this.buttonEditar.createElButton()}
+            <h4 class="card__heading">
+                <span class="card__heading-span">${this.planeta.nome}</span>
+            </h4>
+        </div>
+        <div class="card__details">
+            <ul>
+                <li>CLIMA: ${this.planeta.clima}</li>
+                <li>POPULAÇÃO: ${this.planeta.populacao}</li>
+                <li>DIÂMETO: ${this.planeta.diametro}</li>
+                <li>TEMPO ROTAÇÃO: ${this.planeta.rotacao}</li>
+                <li>TEMPO TRANSLAÇÃO: ${this.planeta.translacao}</li>
+            </ul>
+        </div>
+    </div>
+    <div class="card__side card__side--front js-card-front">
+        <div class="card__theme">
+            <div class="card__theme-box">
+                <p class="card__subject">Planetas</p>
+                <p class="card__title">${this.planeta.nome}</p>
+            </div>
+        </div>
+    </div>`
+    }
+}
 class Planeta {
     constructor(nome, clima, populacao, diametro, rotacao, translacao) {
         this.nome = nome;
@@ -9,32 +58,87 @@ class Planeta {
     }
 }
 
-//ARRUMAR FUNÇÃO GERADORA!!!
-function* guardaEstadoPaginacao(i = 1) {
-    yield i;
-    yield i + 1;
+class Mensagem {
+    constructor(classe, textContent) {
+        this.tag = 'span'
+        this.classe = classe;
+        this.textContent = textContent;
+    }
+    createElMensagem() {
+        return `<${this.tag} class="${this.classe}">${this.textContent}</${this.tag}>`
+    }
+}
+
+class Input {
+    constructor(classe, type, value, funcao) {
+        this.tag = 'input'
+        this.classe = classe;
+        this.type = type;
+        this.value = value;
+        this.funcao = funcao;
+    }
+    createElInput() {
+        return `<${this.tag} type="${this.type}" class="${this.classe}" value="${this.value}" onfocusout="${this.funcao}"></${this.tag}>`
+    }
+}
+
+const addRemover = (element) => {
+    const card = element.parentNode.parentNode.parentNode;
+    card.remove();
+}
+
+const editLine = (element) => {
+    const li = element.parentNode;
+    li.textContent += element.value;
+    element.remove();
+}
+
+const addEditar = (element) => {
+    const card = element.parentNode.parentNode.parentNode;
+    const backInfo = card.querySelector('.js-card-back');
+    backInfo.querySelectorAll('li').forEach(element => {
+        const input = new Input('card-input', 'text', element.textContent.split(': ')[1], "editLine(this)");
+        element.innerHTML = `${element.textContent.split(': ')[0]}: ${input.createElInput()}`;
+    });
+    card.addEventListener('mouseleave', (ev) => {
+        card.querySelectorAll('input').forEach(input => {
+            editLine(input);
+        });
+    })
+}
+
+//FUNÇÃO GERADORA!!!
+function* guardaEstadoPaginacao() {
+    let index = 2;
+    while (true) yield index++;
+}
+
+const geradora = guardaEstadoPaginacao();
+
+
+const createLinkPaginacao = (numero) => {
+    const buttton = new Button('js-pagina-button', numero, 'mostrarMais(this, ' + numero * 10 + ')');
+    const pages = document.querySelector('.js-pages');
+    pages.innerHTML += buttton.createElButton();
 }
 
 async function recuperarDadosSw(url) {
     try {
         const promise = await fetch(url);
         const json = await promise.json();
-        console.log(json.next);
-        if (json.next !== "null") {
-            //ARRUMAR FUNÇÃO GERADORA!!!
-            const nrProximaPagina = guardaEstadoPaginacao().next();
-            console.log(nrProximaPagina);
+        if (json.next !== null) {
+            const nrProximaPagina = geradora.next().value;
             return {
                 json: json,
                 pagina: nrProximaPagina
             };
         } else {
+            geradora.return(false);
             return {
                 json: json,
                 pagina: null
             };
         }
-
     } catch (error) {
         const erro = false;
         return erro;
@@ -42,15 +146,15 @@ async function recuperarDadosSw(url) {
 }
 
 async function criaCards(paginacao) {
-    const msg = document.querySelector('.js-msgRequisicao');
-    msg.textContent = "Carregando...";
     const resultado = await recuperarDadosSw(`https://swapi.dev/api/planets/?page=${paginacao}`);
 
-    if (resultado == false) {
-        msg.textContent = "Erro ao realizar carregamento";
-        return;
+    if (resultado != false) {
+        const mensagem = new Mensagem("mensagem_requisicao js-msgRequisicao", "Carregando...");
+        msgPai.innerHTML = mensagem.createElMensagem();
     } else {
-        //msg.remove();
+        const mensagem = new Mensagem("mensagem_requisicao js-msgRequisicao", "Erro ao realizar carregamento");
+        msgPai.innerHTML = mensagem.createElMensagem();
+        return;
     }
 
     const planetas = resultado.json.results;
@@ -67,36 +171,16 @@ async function criaCards(paginacao) {
         );
         let card = document.createElement('div');
         card.setAttribute('class', 'card js-card');
-        card.innerHTML = `      <div class="card__side card__side--back">
-                                        <div class="card__cover">
-                                            <button class="card__heading-button js-editar">Editar</button>
-                                            <button class="card__heading-button js-excluir">Excluir</button>
-                                            <h4 class="card__heading">
-                                                <span class="card__heading-span">INFO</span>
-                                            </h4>
-                                        </div>
-                                        <div class="card__details">
-                                            <ul>
-                                                <li>CLIMA: ${planeta.clima}</li>
-                                                <li>POPULAÇÃO: ${planeta.populacao}</li>
-                                                <li>DIÂMETO: ${planeta.diametro}</li>
-                                                <li>TEMPO ROTAÇÃO: ${planeta.rotacao}</li>
-                                                <li>TEMPO TRANSLAÇÃO: ${planeta.translacao}</li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                    <div class="card__side card__side--front">
-                                        <div class="card__theme">
-                                            <div class="card__theme-box">
-                                                <p class="card__subject">Planetas</p>
-                                                <p class="card__title">${planeta.nome}</p>
-                                            </div>
-                                        </div>
-                                    </div>`;
+
+        const cardElement = new Card(planeta);
+        card.innerHTML = `${cardElement.createElCard()}`;
         artboard.append(card);
     });
-    if (resultado.pagina !== null)
-        criaCards(resultado.pagina);
+
+    createLinkPaginacao(paginacao);
+
+    if (resultado.pagina !== null) criaCards(resultado.pagina);
+    if (msgPai.hasChildNodes()) msgPai.firstChild.remove();
 }
 
 criaCards(1);
